e4f165ce36d910ab52c5289ed11f584bf79e72f6
gabriellooser2 <gabriel.looser@fhsstudents.ch>
2019-11-25 11:21:41 +0100

    origin

---
+++ routes/users.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var chatHistory = [];
+var nickname = [];
+
+/* GET users listing. */
+
+router.get('/', function (req, res, next) {
+  res.json({ message: 'fhs chat-app api works...' });
+  });
+  router.get('/history', function (req, res, next) {
+  res.send(chatHistory);
+  });
+  router.post('/history', function (req, res, next) {
+  var date = new Date();
+  chatHistory.push({ message: req.body.message, nickname: req.body.nickname });
+  res.json({ message: 'History created!' });
+  });
+
+
+module.exports = router;
